7755b586f176111f804d02e09db95841b5808ce4
cryptoprofitswitcher <cryptoprofitswitcher@protonmail.com>
2018-10-17 17:59:12 +0200

    Fix build BATs

--- PublishUbuntu.bat
+++ PublishUbuntu.bat
@@ -1,1 +1,1 @@
-﻿dotnet publish -c ReleaseLinux -r ubuntu.14.04-x64
+dotnet publish -c ReleaseLinux -r ubuntu.14.04-x64 -o ./bin/publish/CryptonightProfitSwitcher_ubuntu64
---
+++ PublishWindows.bat
@@ -0,0 +1,1 @@
+dotnet publish -c Release -r win-x64 -o ./bin/publish/CryptonightProfitSwitcher_win64
